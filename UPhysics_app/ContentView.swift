335482a1656fb3717015542319651fa0236e06a1
Luiz <luizhenriquesb21@gmail.com>
2022-11-03 15:31:01 -0300

    daniel gente boa

--- UPhysics_app/ContentView.swift
+++ UPhysics_app/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .font(.largeTitle)
                 .padding()
-            Text("Olá mundo!")
+            Text("Daniel cagão <3 !")
                 .font(.largeTitle)
         }
         .padding()
