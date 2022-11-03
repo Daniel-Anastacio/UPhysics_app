a99d19e55e45d7a4acc7767a67e8322b5328e2e5
Luiz <luizhenriquesb21@gmail.com>
2022-11-03 14:36:15 -0300

    Initial Commit

---
+++ UPhysics_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UPhysics_app
+//
+//  Created by Luiz Henrique da Silva Bezerra on 03/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UPhysics_app/UPhysics_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  UPhysics_appApp.swift
+//  UPhysics_app
+//
+//  Created by Luiz Henrique da Silva Bezerra on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UPhysics_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
